7a98185ebc4915d048246806de995575f8d6ee0f
Rajiv Krishnakumar <rajkk1@yahoo.com>
2020-04-05 19:07:23 -0400

    Update test_utils.py

--- ryuenforcement/tests/test_utils.py
+++ ryuenforcement/tests/test_utils.py
@@ -1,7 +1,7 @@
 from common.utils import ACTION
 
 
-def test_actions():
+def test_action_list():
     """All actions are 1-hot encodings of length 12 (having all 0s is also a valid move for 'do nothing')"""
     for _, value in ACTION.items():
         assert len(value) == 12
